a4a334a3fc1bd608d1869b121d307533c2533834
CainChu <cain@cainplay.com>
2017-09-22 20:53:53 +0800

    dev

--- js/bbPool.js
+++ js/bbPool.js
@@ -1,12 +1,14 @@
 function BB_Pool(num) {
     this.pool = []
+    this.num = num
     while (num-- > 0) {
         this.pool.push(new BB())
     }
 }
 
 BB_Pool.prototype.fire = function (me) {
-    if (this.pool.length > 0) {
+    if (this.num > 0) {
+        this.num--
         let bb = this.pool.pop()
         bb.position = me.position
         bb.inStage(true)
@@ -16,4 +18,5 @@ BB_Pool.prototype.fire = function (me) {
 BB_Pool.prototype.recove = function (bb) {
     bb.inStage(false)
     this.pool.push(bb)
+    this.num++
 }
