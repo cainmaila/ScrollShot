987c0a4c819b21c4424bc09fa1ee3b91d7609f24
CainChu <cainmaila@gmail.com>
2017-11-21 17:58:23 +0800

    demo

--- readDom/inherit.js
+++ readDom/inherit.js
@@ -12,18 +12,36 @@ MyClass.prototype.b = function(val) {
 
 var NewClass = function(c) {
     MyClass.call(this, c)
+    // this.__proto__.constructor(c)
     // this.constructor(c)
 }
-
+NewClass.prototype = Object.create(MyClass.prototype)
+// NewClass.constructor = MyClass.constructor
 NewClass.prototype.b = function(c) {
     console.log('NewClass b val = ' + (c + 5))
 }
-
-NewClass.prototype = Object.create(MyClass.prototype)
-// NewClass.prototype = new MyClass(3)
-// NewClass.prototype.constructor = MyClass.constructor
 
 var newObj = new NewClass(1)
 
 console.log(newObj.a)
 newObj.b(2)
+
+// class A {
+//     constructor(a) {
+//         this.a = a
+//     }
+//     b() {
+//         console.log('b', this.a)
+//     }
+// }
+
+// class B extends A {
+//     constructor(a) {
+//         super(a)
+//     }
+//     b() {
+//         console.log('b2', this.a)
+//     }
+// }
+
+// new B(123)
